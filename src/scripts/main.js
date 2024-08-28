a1f9238b4a3fbfd65027bf100476c26c60a2df2e
Roma <rasevychr@gmail.com>
2024-08-28 19:43:21 +0300

    sol4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,8 @@
 'use strict';
 
-const inputs = document.querySelectorAll('.contacts__input, .contacts__input-textarea');
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-inputs.forEach(input => {
-  const placeholder = input.getAttribute('placeholder');
-
-  input.addEventListener('focus', () => {
-    if (!input.value) {
-      input.value = placeholder;
-      input.setSelectionRange(placeholder.length, placeholder.length);
-    }
-  });
-
-  input.addEventListener('blur', () => {
-    if (input.value === placeholder) {
-      input.value = '';
-    }
-  });
+  this.reset();
 });
 
